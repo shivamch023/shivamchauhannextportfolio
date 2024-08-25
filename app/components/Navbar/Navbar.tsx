f59cc0544b8aeb1b76b6643e1a5289512d73533e
shivamch023 <shivamchauhan2499@gmail.com>
2024-08-26 00:39:30 +0530

    navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -2,28 +2,34 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import { IoIosDownload } from "react-icons/io";
+// import { IoIosDownload } from "react-icons/io";
 import { FiHome, FiFileText, FiBook, FiPhone } from "react-icons/fi";
 import Image from "next/image";
 import { CiDark } from "react-icons/ci";
 
 function NavBar() {
-  const [activeLink, setActiveLink] = useState("");
+  const [activeLink, setActiveLink] = useState("home");
 
   return (
     <div className="container">
-      <nav className="w-full  fixed top-0 left-0 right-0 z-20 bg-[#0E2F44] shadow-lg py-4">
-        <div className="flex border justify-between items-center w-full px-4 mx-auto lg:max-w-7xl md:px-8">
-          <Link
-            href="/"
-            className="border flex justify-between items-center p-2  rounded-full"
-          >
+      <nav className="w-full   fixed top-0 left-0 right-0 z-20 bg-[#0E2F44] shadow-lg py-4">
+        <div className="flex rounded-[30px] py-0 transparent border justify-between items-center w-full px-4 mx-auto lg:max-w-7xl md:px-8">
+          <h2 className="border flex justify-between items-center p-2  rounded-full">
             SC
-          </Link>
+          </h2>
 
           <div className="hidden md:flex space-x-6">
             <Link
-              href="#about"
+              href="/"
+              className={`text-[#FFFFFF] ${
+                activeLink === "home" ? "font-semibold" : ""
+              }`}
+              onClick={() => setActiveLink("home")}
+            >
+              Home
+            </Link>
+            <Link
+              href="/about"
               className={`text-[#FFFFFF] ${
                 activeLink === "about" ? "font-semibold" : ""
               }`}
@@ -31,26 +37,18 @@ function NavBar() {
             >
               About
             </Link>
+
             <Link
-              href="#projects"
+              href="/portfolio"
               className={`text-[#FFFFFF] ${
-                activeLink === "projects" ? "font-semibold" : ""
+                activeLink === "portfolio" ? "font-semibold" : ""
               }`}
-              onClick={() => setActiveLink("projects")}
-            >
-              Projects
-            </Link>
-            <Link
-              href="#blog"
-              className={`text-[#FFFFFF] ${
-                activeLink === "blog" ? "font-semibold" : ""
-              }`}
-              onClick={() => setActiveLink("blog")}
+              onClick={() => setActiveLink("portfolio")}
             >
               Portfolio
             </Link>
             <Link
-              href="#contact"
+              href="/contact"
               className={`text-[#FFFFFF] ${
                 activeLink === "contact" ? "font-semibold" : ""
               }`}
@@ -59,20 +57,20 @@ function NavBar() {
               Contact
             </Link>
             <Link
-              href="#contact"
+              href="/skill"
               className={`text-[#FFFFFF] ${
-                activeLink === "contact" ? "font-semibold" : ""
+                activeLink === "skill" ? "font-semibold" : ""
               }`}
-              onClick={() => setActiveLink("contact")}
+              onClick={() => setActiveLink("skill")}
             >
-              Skills
+              Tech Stack
             </Link>
             <Link
-              href="#contact"
+              href="/service"
               className={`text-[#FFFFFF] ${
-                activeLink === "contact" ? "font-semibold" : ""
+                activeLink === "service" ? "font-semibold" : ""
               }`}
-              onClick={() => setActiveLink("contact")}
+              onClick={() => setActiveLink("service")}
             >
               Services
             </Link>
@@ -85,81 +83,93 @@ function NavBar() {
       </nav>
 
       <div className="md:hidden fixed top-0 left-0 right-0 z-20 bg-[#0E2F44] p-4 flex justify-between items-center shadow-md">
-        <Link
-          href="/"
-          className="border flex justify-between items-center p-2  rounded-full"
-        >
+        <h2 className="border flex justify-between items-center p-2  rounded-full">
           SC
-        </Link>
+        </h2>
         <Link href="/modal">
           <CiDark />
         </Link>
       </div>
 
       <div className="fixed bottom-0 left-0 right-0 z-20 bg-[#0E2F44] shadow-t-md p-4 md:hidden">
-        <ul className="flex justify-between gap-2 items-center relative">
+        <ul className="flex justify-between  items-center relative">
           <li
             className={`text-center flex items-center justify-center ${
-              activeLink === "about" ? "text-[#40E0D0]" : "text-[#FFFFFF]"
+              activeLink === "home" ? "text-[#40E0D0]" : "text-[#FFFFFF]"
             }`}
           >
-            <Link href="#about" onClick={() => setActiveLink("about")}>
+            <Link href="/" onClick={() => setActiveLink("home")}>
               <div className="flex flex-col items-center">
                 <div
                   className={`p-2 rounded-full transition-colors duration-300 ease-in-out ${
-                    activeLink === "about"
+                    activeLink === "home"
                       ? "bg-[#40E0D0]/50" // Light transparent background
                       : ""
                   }`}
                 >
-                  <FiHome size={20} />
+                  <FiHome size={15} />
                 </div>
-                {/* <span className="mt-1 text-sm">About</span> */}
+                <span className="mt-0 text-[10px]">Home</span>
               </div>
             </Link>
           </li>
           <li
             className={`text-center flex items-center justify-center ${
-              activeLink === "blog" ? "text-[#40E0D0]" : "text-[#FFFFFF]"
+              activeLink === "portfolio" ? "text-[#40E0D0]" : "text-[#FFFFFF]"
             }`}
           >
-            <Link href="#blog" onClick={() => setActiveLink("blog")}>
-              <div className="flex flex-col items-center ml-[-50%]">
+            <Link href="/portfolio" onClick={() => setActiveLink("portfolio")}>
+              <div className="flex flex-col items-center ml-[-60%] ">
                 <div
                   className={`p-2 rounded-full transition-colors duration-300 ease-in-out ${
-                    activeLink === "blog" ? "bg-[#40E0D0]/50" : ""
+                    activeLink === "portfolio" ? "bg-[#40E0D0]/50" : ""
                   }`}
                 >
-                  <FiBook size={20} />
+                  <FiBook size={15} />
                 </div>
-                {/* <span className="mt-1 text-sm">Portfolio</span> */}
+                <span className="mt-0 text-[10px]">Portfolio</span>
               </div>
             </Link>
           </li>
-          <li className="p-2 h-[70px] w-[70px] absolute top-[-50%] left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-45 border-t-4 rounded-full border-l-4 border-[#0e2f44] ">
-            <Image
-              src="/assets/shivam profile.jpg"
-              className="border -rotate-45 absolute top-[49%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full"
-              alt=""
-              height={70}
-              width={70}
-            />
+          <li className="p-2 h-[70px] bg-[#0e2f44] w-[70px] absolute top-[-50%] left-1/2 -translate-x-[55%] translate-y-[-47%] rotate-45 border-t-4 rounded-full border-l-4 border-[#0e2f44] ">
+            <Link
+              href="/about"
+              onClick={() => setActiveLink("about")}
+              title="About"
+            >
+              <div
+                className={`p-2 rounded-full border transition-colors duration-300 ease-in-out ${
+                  activeLink === "about"
+                    ? "bg-[#40E0D0]/50 border-b-4 rounded-b-full"
+                    : ""
+                }`}
+              >
+                <Image
+                  src="/assets/shivam profile.jpg"
+                  className="-rotate-45 absolute top-[49%] left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full"
+                  alt=""
+                  height={70}
+                  width={70}
+                />
+              </div>
+            </Link>
           </li>
+
           <li
             className={`text-center flex items-center justify-center ${
-              activeLink === "projects" ? "text-[#40E0D0]" : "text-[#FFFFFF]"
+              activeLink === "project" ? "text-[#40E0D0]" : "text-[#FFFFFF]"
             }`}
           >
-            <Link href="#projects" onClick={() => setActiveLink("projects")}>
-              <div className="flex flex-col items-center ml-[50%]">
+            <Link href="/project" onClick={() => setActiveLink("project")}>
+              <div className="flex flex-col items-center ml-[60%]">
                 <div
                   className={`p-2 rounded-full transition-colors duration-300 ease-in-out ${
-                    activeLink === "projects" ? "bg-[#40E0D0]/40" : ""
+                    activeLink === "project" ? "bg-[#40E0D0]/40" : ""
                   }`}
                 >
-                  <FiFileText size={20} />
+                  <FiFileText size={15} />
                 </div>
-                {/* <span className="mt-1 text-sm">Projects</span> */}
+                <span className="mt-0 text-[10px]">Projects</span>
               </div>
             </Link>
           </li>
@@ -168,16 +178,16 @@ function NavBar() {
               activeLink === "contact" ? "text-[#40E0D0]" : "text-[#FFFFFF]"
             }`}
           >
-            <Link href="#contact" onClick={() => setActiveLink("contact")}>
+            <Link href="/contact" onClick={() => setActiveLink("contact")}>
               <div className="flex flex-col items-center">
                 <div
                   className={`p-2 rounded-full transition-colors duration-300 ease-in-out ${
                     activeLink === "contact" ? "bg-[#40E0D0]/50" : ""
                   }`}
                 >
-                  <FiPhone size={20} />
+                  <FiPhone size={15} />
                 </div>
-                {/* <span className="mt-1 text-sm">Contact</span> */}
+                <span className="mt-0 text-[10px]">Contact</span>
               </div>
             </Link>
           </li>
