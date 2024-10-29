94ce250b7fed93443f4035a5d34c2485c3313f4d
shiavm chauhan <shivamchauhan2499@gmail.com>
2024-10-29 21:36:09 +0530

    added responsive

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -55,14 +55,14 @@ const ProjectDetailPage = () => {
   if (!project) return <div>No project found.</div>; // Handle case where project is null
 
   return (
-    <div className="container flex flex-col w-full mx-auto pt-[6rem] gap-[2rem] pb-[2rem] px-[2rem]">
+    <div className="container flex flex-col w-full mx-auto pt-[3rem] xl:pt-[6rem] md:pt-[6rem]  gap-[2rem] pb-[2rem] px-[0.8rem] xl:px-[2rem]">
       <div className="flex justify-center items-center">
-        <h1 className="text-[2.5rem] text-nowrap font-bold mb-[2.5rem] text-[#008080]">
+        <h1 className="text-[1.8rem] xl:text-[2.5rem] md:text-[2.5rem] text-nowrap font-bold mb-[2.5rem] text-[#008080]">
           {project.title}
         </h1>
       </div>
-      <div className="w-full flex justify-between">
-        <div className="w-[65%] h-[395px] border border-gray-600 p-2 rounded-[10px]">
+      <div className="w-full flex flex-col justify-between xl:flex-row md:flex-col xl:gap-0  gap-4">
+        <div className="w-full xl:w-[65%] xl:h-[395px] h-[200px] md:w-full border border-gray-600 p-2 rounded-[10px]">
           <Image
             src={project.image}
             alt={project.title}
@@ -71,7 +71,7 @@ const ProjectDetailPage = () => {
             height={1000}
           />
         </div>
-        <div className="w-[33%] h-[395px] flex flex-col gap-[2rem] border rounded-[10px] px-[1rem] py-[2rem] border-gray-600">
+        <div className="w-full xl:w-[33%] md:w-full h-[395px] xl:h-[395px] md:h-[320px] flex flex-col gap-[2rem] border rounded-[10px] px-[1rem] py-[2rem] border-gray-600">
           <div className="flex items-center justify-center gap-5">
             <Link
               href={project.github}
@@ -104,7 +104,7 @@ const ProjectDetailPage = () => {
         </div>
       </div>
       <div className="flex border border-gray-600 flex-col gap-5 px-6 py-6 rounded-[10px]">
-        <h2 className="text-[1.8rem] gap-2 text-white font-bold flex items-center">
+        <h2 className="text-[1.5rem] xl:text-[1.8rem] gap-2 text-white font-bold flex items-center">
           Skills Used <GiSkills className="text-red-500" />
         </h2>
         <ul className="flex flex-wrap  gap-[1rem] ml-5 text-[#d5d5d5]">
@@ -112,13 +112,13 @@ const ProjectDetailPage = () => {
             project.skills.map((skill, index) => (
               <li
                 key={index}
-                className="border text-[1.2rem] w-[12rem] text-center h-[3rem] flex items-center justify-center rounded-[10px] text-white py-1 px-6 border-gray-600 hover:bg-gray-900 transition-all text-nowrap"
+                className="border text-[1.2rem] w-full xl:w-[12rem] text-center h-[3rem] flex items-center justify-center rounded-[10px] text-white py-1 px-6 border-gray-600 hover:bg-gray-900 transition-all text-nowrap"
               >
                 {skill}
               </li>
             ))
           ) : (
-            <li>No skills available</li> // Handle case where skills are undefined or empty
+            <li>No skills available</li>
           )}
         </ul>
       </div>
