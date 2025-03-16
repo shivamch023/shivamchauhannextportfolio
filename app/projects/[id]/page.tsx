3b0165a15a46a046ff93b1472b98b4efecb911c2
shiavm chauhan <shivamchauhan2499@gmail.com>
2025-03-16 11:45:56 +0530

    added

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -52,9 +52,6 @@ const ProjectDetailPage = () => {
   if (loading)
     return (
       <div className="flex flex-col justify-center items-center h-screen bg-gray-900">
-        <div className="loader w-24 h-24 border-8 border-t-8 border-teal-400 rounded-full animate-spin relative">
-          <div className="absolute inset-0 rounded-full border-8 border-teal-400 opacity-50 blur-lg"></div>
-        </div>
         <div className="flex items-center justify-center min-h-screen bg-transparent">
           <div className="relative w-16 h-16">
             <div className="absolute inset-0 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
